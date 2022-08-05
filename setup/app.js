aa8acb6887e63d4b3e13caf21e2dfe55fcc86966
AdeshDaniel <stockhutfxcompany@gmail.com>
2022-08-05 14:24:23 +0100

    final commit

--- setup/app.js
+++ setup/app.js
@@ -26,7 +26,14 @@ const giveaway = document.querySelector(".giveaway");
 const deadline = document.querySelector(".deadline");
 const items = document.querySelectorAll(".deadline-format h4");
 
-let futureDate = new Date(2022, 7, 24, 18, 30, 0);
+let tempDate = new Date();
+let tempYear = tempDate.getFullYear();
+let tempMonth = tempDate.getMonth();
+let tempDay = tempDate.getDay();
+
+// let futureDate = new Date(2022, 7, 24, 18, 30, 0);
+
+const futureDate = new Date(tempYear, tempMonth, tempDay + 11, 30, 0);
 
 const year = futureDate.getFullYear();
 const hours = futureDate.getHours();
