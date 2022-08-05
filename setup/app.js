f261c6543372ee34e64406d9182032559d135fa8
AdeshDaniel <stockhutfxcompany@gmail.com>
2022-08-05 14:12:47 +0100

    my final commit

--- setup/app.js
+++ setup/app.js
@@ -21,3 +21,68 @@ const weekdays = [
   "Friday",
   "Saturday",
 ];
+
+const giveaway = document.querySelector(".giveaway");
+const deadline = document.querySelector(".deadline");
+const items = document.querySelectorAll(".deadline-format h4");
+
+let futureDate = new Date(2022, 7, 24, 18, 30, 0);
+
+const year = futureDate.getFullYear();
+const hours = futureDate.getHours();
+const mins = futureDate.getMinutes();
+
+let month = futureDate.getMonth();
+month = months[month];
+
+const date = futureDate.getDate();
+const day = weekdays[futureDate.getDay()];
+
+giveaway.textContent = `giveaway ends on ${day}, ${date} ${month} ${year} ${hours}:${mins}pm`;
+
+const futureTime = futureDate.getTime();
+
+function getRemainingTime() {
+  const today = new Date().getTime();
+  const t = futureTime - today;
+  // 1s = 1000ms
+  // 1m = 60min
+  // 1hr = 60sec
+  // 1d = 24hrs
+
+  // values in ms
+  const oneDay = 24 * 60 * 60 * 1000;
+  const oneHour = 60 * 60 * 1000;
+  const oneMins = 60 * 1000;
+  let days = t / oneDay;
+  days = Math.floor(days);
+
+  let hour = Math.floor((t % oneDay) / oneHour);
+
+  let mins = Math.floor((t % oneHour) / oneMins);
+
+  let secs = Math.floor((t % oneMins) / 1000);
+
+  // set values array
+  const values = [days, hours, mins, secs];
+
+  function format(item) {
+    if (item < 10) {
+      return (item = `0${item}`);
+    }
+    return item;
+  }
+
+  items.forEach(function (item, index) {
+    item.innerHTML = format(values[index]);
+  });
+  if (t < 0) {
+    clearInterval(countdown);
+    deadline.innerHTML = `<h4 class="expired">Sorry, this giveaway has expired</h4>`;
+  }
+}
+
+// countdown
+let countdown = setInterval(getRemainingTime, 1000);
+
+getRemainingTime();
